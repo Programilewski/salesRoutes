1900c1aac8136124a3bebcaaee9373f31436ba94
Programilewski <pawel.milewski00@gmail.com>
2024-05-13 13:07:04 +0200

    Add CORS header in salesmen.php file

--- controllers/API/salesmen.php
+++ controllers/API/salesmen.php
@@ -7,6 +7,7 @@ $db = new Database($config, "izak", "103@Ny4ff");
 $db->query("SELECT * FROM salesmen");
 $data = $db->fetchAllDataAssoc();
 
+header("Access-Control-Allow-Origin: http://routesopti.infinityfreeapp.com/");
 header("Content-Type:application/json");
 
 echo json_encode($data);
