08a4263d980b9308954a7510dc824f1166a438bf
Pawel Milewski <pawel.milewski00@gmail.com>
2024-05-27 18:36:19 +0200

    Add the config file to gitignore

--- src/Database.php
+++ src/Database.php
@@ -4,15 +4,15 @@ class Database
 {
     public $connection;
     public $statement;
-    public function __construct($config, $username = "izak", $password = "103@Ny4ff")
+    public function __construct($config, $username = "root", $password = "")
     {
         $dsn = "mysql:" . http_build_query($config, "", ";");
         $this->connection = new PDO($dsn, $username, $password);
     }
-    public function query($query,$params = [])
+    public function query($query, $params = [])
     {
         $this->statement  = $this->connection->prepare($query);
-//        $this->statement->execute($params);
+        //        $this->statement->execute($params);
         $this->statement->execute($params);
         return $this;
     }
